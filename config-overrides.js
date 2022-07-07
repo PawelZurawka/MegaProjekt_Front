0e2e0d11e1c2942fea27c498d817a0d738a84618
Paweł Żurawka <pzurawka@gmail.com>
2022-07-07 18:29:06 +0200

    Change conception.

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-const {override} = require('customize-cra');
-const {aliasWebpack, configPaths} = require('react-app-alias-ex');
-
-module.exports = {
-	webpack: override(
-		aliasWebpack(configPaths('./tsconfig.paths.json'))
-	),
-}

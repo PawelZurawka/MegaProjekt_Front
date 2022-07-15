0e1f6899f49bb4d97590140dcae9824cb2126a62
Paweł Żurawka <pzurawka@gmail.com>
2022-07-15 20:42:57 +0200

    Add config override.

---
+++ src/config-overrides.js
@@ -0,0 +1,6 @@
+const { override } = require('customize-cra');
+const { aliasWebpack, configPaths } = require('react-app-alias-ex');
+
+module.exports = {
+  webpack: override(aliasWebpack(configPaths('./tsconfig.paths.json'))),
+};
